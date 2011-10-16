ef0d2a79fb02a01fba57b0fc2c4c2c1e52818fed
rene <rene@renes-MacBook-Pro.local>
2011-10-15 20:32:37 -0500

    added basic support monitoring specific queues

--- app.rb
+++ app.rb
@@ -6,3 +6,8 @@ use Faye::RackAdapter, :mount => '/faye', :timeout => 25
 get '/' do
   erb :index
 end
+
+get '/queue/:id' do
+  @queue_name = params[:id]
+  erb :queue
+end
--- events.rb
+++ events.rb
@@ -1,12 +1,19 @@
 require 'bayeux'
 
 events.asterisk.manager_interface.each do |event|
+  ahn_log.events "#{event.name} - - #{event.inspect}"
   case event.name
   when 'Join'
     Bayeux.publish('/caller/join', { :id => event.headers['Channel'], 
                                      :number => event.headers['CallerIDNum'],
                                      :joinedAt => Time.now.to_i })
+
+    Bayeux.publish("/#{event.headers['Queue']}/join", { :id => event.headers['Channel'],
+                                     :number => event.headers['CallerIDNum'],
+                                     :joinedAt => Time.now.to_i })
   when 'Leave'
     Bayeux.publish('/caller/leave', { :id => event.headers['Channel'] })
+
+    Bayeux.publish("/#{event.headers['Queue']}/leave", { :id => event.headers['Channel'] })
   end
 end
